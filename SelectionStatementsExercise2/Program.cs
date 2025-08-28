86f2d22b88f6e5a163c05bf943841b9170f4e1c2
Andrew Olson <andruolson@gmail.com>
2025-08-28 16:30:14 -0600

    Did exercise two with the 5 different school subject option

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hey, what's your favorite subject in school?");
+            
+            string userChoise = Console.ReadLine();
+
+            switch (userChoise.ToUpper())
+            {
+                case "MATH":
+                    Console.WriteLine("Gross, I hate math...");
+                    break;
+                case "SCIENCE":
+                    Console.WriteLine("Nerd!");
+                    break;
+                case "WRITING":
+                    Console.WriteLine("You'd think because I can read, I should be able to write..");
+                    break;
+                case "SPORTS":
+                    Console.WriteLine("I'm more of a chess guy.. but sports are cool");
+                    break;
+                case "ART":
+                    Console.WriteLine("I like doing art too!");
+                    break;
+                default:
+                    Console.WriteLine("Ok....");
+                    break;
+                    
+            }
         }
     }
 }
