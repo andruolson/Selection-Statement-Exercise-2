8b1ba312c11f12589687e27dcb8d72df6ba2aa2e
Andrew Olson <andruolson@gmail.com>
2026-02-24 23:34:51 -0700

    did exercise 2, using a switch case selection statement.

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+            string favSubject = Console.ReadLine();
+
+            switch (favSubject)
+            {
+                case "Math":
+                    Console.WriteLine("Nice, I like math...");
+                    break;
+                case "Biology":
+                    Console.WriteLine("Nice, I like biology...");
+                    break;
+                case "History":
+                    Console.WriteLine("Lame..., I'm not into history...");
+                    break;
+                case "Science":
+                    Console.WriteLine("NERD");
+                    break;
+                case "Art":
+                    Console.WriteLine("Awesome, I love art!");
+                    break;
+                default:
+                    Console.WriteLine("Something went wrong...");
+                    break;
+            }
         }
     }
 }
